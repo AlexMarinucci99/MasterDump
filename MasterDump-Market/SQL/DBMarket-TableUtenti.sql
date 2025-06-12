02a5d2a45b7a9a8546a897178f9f2bad2ae43e97
master <161362468+AlexMarinucci99@users.noreply.github.com>
2025-06-12 17:10:46 +0200

    up

--- MasterDump-Market/SQL/DBMarket-TableUtenti.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE MasterDumpMarket;
-USE MasterDumpMarket;
-CREATE TABLE Utenti (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    ruolo ENUM('ordinante', 'tecnico', 'amministratore') NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
